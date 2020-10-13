e3c9db751ac28397c3c9e18a6801d17ed9cc25b7
daoson12 <daoduvictor48@gmail.com>
2020-10-13 14:46:30 -0700

    updated settings

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { SignInComponent } from './security/sign-in/sign-in.component';
 
 
 
 const routes: Routes = [
-  {path:'sign-up', component: SignInComponent}
+  {}
 ];
 
 @NgModule({
